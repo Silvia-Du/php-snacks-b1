745c5d0c5bc00fe78d81630a178e2159b22526fb
Silvia-Du <silviadu@hotmail.it>
2022-06-07 15:35:13 +0200

    Snack 5: Separato e stampato in pagina i paragrafi, con 1' metodo di verifica per par.vuoti

--- Snack5/index.php
+++ Snack5/index.php
@@ -5,6 +5,12 @@ Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il pa
 aggiungete poi i seguenti snack da realizzare
 */
 
+$text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam animi aperiam consectetur quas voluptates quo maxime! Accusantium enim eaque ea quis voluptate, nam sunt exercitationem? Voluptatem non molestias similique eaque quod natus laborum aliquid ipsa eum cum. Non, sapiente cum soluta explicabo voluptatem debitis aliquam molestias ab error ex rerum, animi corporis qui excepturi perferendis obcaecati fugiat facere reiciendis? Autem est consequuntur eveniet ipsum inventore distinctio molestias labore in dolorem dicta. Magni repellendus eaque assumenda et laborum. Rem cupiditate possimus beatae qui consectetur labore numquam amet. Quas necessitatibus aut error sed tempora beatae pariatur. Libero sit architecto doloremque inventore unde.';
+
+$paragraphs = explode('.', $text);
+
+var_dump($paragraphs)
+
 ?>
 
 <!DOCTYPE html>
@@ -16,6 +22,20 @@ aggiungete poi i seguenti snack da realizzare
   <title>Snack5</title>
 </head>
 <body>
+
+<h2>Testo:</h2>
+
+<p><?php echo $text ?></p>
+
+<h2>Suddivisione in paragrafi</h2>
+
+<ul>
+  <?php for($i = 0; $i < count($paragraphs); $i++ ): ?>
+    <?php if(strlen($paragraphs[$i]) > 0):?>
+      <li><?php echo $paragraphs[$i]?></li>
+    <?php endif; ?>
+  <?php endfor; ?>
+</ul>
   
 </body>
 </html>
