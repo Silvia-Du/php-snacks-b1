64f845b2ce4cccf321d38d25efc4b8ba8023d2ab
Silvia-Du <silviadu@hotmail.it>
2022-06-07 15:06:48 +0200

    Fine Snack2

--- Snack2/index.php
+++ Snack2/index.php
@@ -4,6 +4,39 @@
 Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
 */
 
+$name = $_GET['name'];
+$mail = $_GET['mail'];
+$age = $_GET['age'];
+var_dump($_GET);
+$check = true;
+$errorType = '';
+
+if(empty($name) || empty($mail) || empty($age)){
+  $check = false;
+  $errorType = 'uno dei dati è mancante';
+
+}elseif(strlen($name) < 3){
+ $check = false;
+ $errorType = 'il nome è troppo corto';
+
+}elseif(!strpos($mail, '@') || !strpos($mail, '.')){
+  $check = false;
+  $errorType = 'la mail non è corretta';
+
+}elseif(!is_numeric($age)){
+  $check = false;
+  $errorType = 'age non è un numero';
+
+}
+
+If($check){
+  $output = 'Accesso riuscito';
+}else{
+  $output = 'Accesso negato';
+}
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -15,6 +48,13 @@ Passare come parametri GET name, mail e age e verificare (cercando i metodi che 
   <title>Snack2</title>
 </head>
 <body>
-  
+
+  <h2>Verifica di accesso:</h2>
+
+  <h3><?php echo $output ?></h3>
+  <?php if(!$check): ?>
+  <h3>Errore di accesso: <?php echo $errorType ?></h3>
+  <?php endif; ?>
+
 </body>
 </html>
